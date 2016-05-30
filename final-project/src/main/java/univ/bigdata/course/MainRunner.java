74a6850151ca1fb8ca7500a843edc1403e2894e9
nirosen <nirosen99@gmail.com>
2016-05-30 22:25:57 +0300

    HW files from moodle 30.5

---
+++ final-project/src/main/java/univ/bigdata/course/MainRunner.java
@@ -0,0 +1,11 @@
+package univ.bigdata.course;
+
+public class MainRunner {
+
+    public static void main(String[] args) {
+        /*
+        TODO: You need to add here all logic required to parse and execute commands.
+         */
+        System.out.println("Hello World.");
+    }
+}
