5b05babb145f8a98c1ee8d89da1fe5dd023d76ca
zahi <zahiyona2007@gmail.com>
2016-06-02 19:28:09 +0300

    added spark dependencies.  start spark  load  file to the RDD

--- final-project/src/main/java/univ/bigdata/course/MainRunner.java
+++ final-project/src/main/java/univ/bigdata/course/MainRunner.java
@@ -1,11 +1,28 @@
 package univ.bigdata.course;
 
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class MainRunner {
 
     public static void main(String[] args) {
         /*
         TODO: You need to add here all logic required to parse and execute commands.
          */
+        SparkConf sparkConf = null;
+        JavaSparkContext sc = null;
+        try {
+            sparkConf = new SparkConf().setAppName("My Spark Job").setMaster("local");
+            sc = new JavaSparkContext(sparkConf);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Failed to create spark context \n" + e.toString());
+        }
+        JavaRDD<String> textFiles = sc.textFile("c:\\movies-simple.txt");
+        //get("/hello", (req, res) -> "Hello World");
         System.out.println("Hello World.");
     }
 }
